740c5b7e854ed953e17a9f9e9e38f5722145b83a
RPS <vincentbusiness55@gmail.com>
2021-12-07 21:41:15 -0800

    Fixing HTML Settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,9 +75,26 @@ myst_enable_extensions = [
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+html_logo = '.../stancium_sphinx.png'
+# html_favicon = '../stancium_sphinx.ico'
+
 html_theme = 'furo'
 
+html_theme_options = {
+    'light_css_variables': {
+        'color-brand-primary': '#4C8CBF',
+        'color-brand-content': '#306998',
+        'color-admonition-background': 'blue',
+    },
+    'dark_css_variables': {
+        'color-brand-primary': '#306998',
+        'color-brand-content': '#FFE871',
+        'color-admonition-background': 'yellow',
+    },
+    "sidebar_hide_name": False,
+}
+# Defaults To Dark Mode
+default_dark_mode = True
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
